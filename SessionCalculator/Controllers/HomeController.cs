099523c9250327b326db55bc841b95a509ad5a2f
kenan <kenan@LAPTOP-5KTTCAE1>
2021-05-27 10:04:25 +0200

    Add project files.

---
+++ SessionCalculator/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SessionCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetInt32("Increment", 0);
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(string click)
+        {
+            int increment = HttpContext.Session.GetInt32("Increment").Value;
+
+            if (click == "Increment")
+            {
+                increment++;
+            }
+            else
+            {
+                if (increment > 0)
+                {
+                    increment--;
+                }
+                else
+                {
+                    ViewBag.Message = "The value cannot be negative";
+                }
+            }
+            HttpContext.Session.SetInt32("Increment", increment);
+
+            return View();
+        }
+
+       
+    }
+}
